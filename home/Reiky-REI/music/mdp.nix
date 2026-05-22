20e29903f47964fd38466c42896cd65ea78553d3
reiky <reiky@stud.tjut.edu.cn>
2026-05-23 00:12:01 +0800

    maybe the home dir is been used so it is raw, so that we can change the username to build a new user

--- home/Reiky-REI/music/mdp.nix
+++ home/Reiky-REI/music/mdp.nix
@@ -4,7 +4,7 @@
   ];
   services.mpd.enable = true;
   services.mpd = {
-    musicDirectory = "/home/reiky/music";
+    musicDirectory = "/home/Reiky-REI/music";
     extraConfig = ''
       audio_output {
         type "pipewire"
@@ -17,9 +17,9 @@
   };
   programs.ncmpcpp = {
     enable = true;
-    mpdMusicDir = "/home/reiky/music";
+    mpdMusicDir = "/home/Reiky-REI/music";
     settings = {
-      lyrics_directory = "/home/reiky/music";
+      lyrics_directory = "/home/Reiky-REI/music";
       store_lyrics_in_song_dir = "yes";
     };
   };
