59cc34fd22381de96d3dbd9c210b28025db71343
reiky <reiky@stud.tjut.edu.cn>
2026-05-23 23:28:46 +0800

    Beautify the system

--- home/Reiky-REI/programs/app.nix
+++ home/Reiky-REI/programs/app.nix
@@ -7,33 +7,32 @@
     youtube-music
     spotify
     kdePackages.dolphin
-    notion-app-enhanced
     netease-cloud-music-gtk
     logseq
   ];
 
   # 谷歌浏览器设置 主题
-  # programs.chromium = {
-  #   enable = true;
-  #   commandLineArgs = [
-  #     "--force-dark-mode"
-  #     "--enable-features=UseOzonePlatform"
-  #     "--ozone-platform=wayland"
-  #     "--force-device-scale-factor=1"
-  #     "--enable-gpu-rasterization"
-  #     "--enable-zero-copy"
-  #   ];
-  #   extensions = [
-  #     "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
-  #     "bpoadfkcbjbfhfodiogcnhhhpibjhbnh" # 沉浸式翻译
-  #     "egehpkpgpgooebopjihjmnpejnjafefi" # Better History | 管理、导出与删除历史记录
-  #     "kjoehdlockiihccmfnoamenhobkkddng" # CSDN自动展开+一键复制代码
-  #   ];
-  # };
-  # catppuccin.chromium = {
-  #   enable = true;
-  #   flavor = "mocha";
-  # };
+  programs.chromium = {
+    enable = true;
+    commandLineArgs = [
+      "--force-dark-mode"
+      "--enable-features=UseOzonePlatform"
+      "--ozone-platform=wayland"
+      "--force-device-scale-factor=1"
+      "--enable-gpu-rasterization"
+      "--enable-zero-copy"
+    ];
+    #   extensions = [
+    #     "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
+    #     "bpoadfkcbjbfhfodiogcnhhhpibjhbnh" # 沉浸式翻译
+    #     "egehpkpgpgooebopjihjmnpejnjafefi" # Better History | 管理、导出与删除历史记录
+    #     "kjoehdlockiihccmfnoamenhobkkddng" # CSDN自动展开+一键复制代码
+    #   ];
+  };
+  catppuccin.chromium = {
+    enable = true;
+    flavor = "mocha";
+  };
 
   # firefox
   programs.firefox = {
