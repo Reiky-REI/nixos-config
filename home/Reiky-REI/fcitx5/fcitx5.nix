3c39b4b3cf0e4c96cb2682e99a7157226f7981bc
reiky <reiky@stud.tjut.edu.cn>
2026-05-23 23:44:04 +0800

    修复了大部分时候无法输入中文的问题

--- home/Reiky-REI/fcitx5/fcitx5.nix
+++ home/Reiky-REI/fcitx5/fcitx5.nix
@@ -1,36 +1,53 @@
-{pkgs, ...}: let
-  qt6pkgs = pkgs.qt6Packages;
-in {
+{pkgs, ...}: {
+  # 选择输入法类型为 fcitx5
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
-    fcitx5.addons = with pkgs; [
-      qt6pkgs.fcitx5-chinese-addons # 修正包名
-      fcitx5-gtk
-    ];
+  };
+
+  # 添加常用中文模块
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    qt6Packages.fcitx5-configtool
+    qt6Packages.fcitx5-chinese-addons # 拼音、五笔等
+  ];
+  i18n.inputMethod.fcitx5.waylandFrontend = true;
+
+  /*
+  *i18n.inputMethod.fcitx5.settings.inputMethod与catppuccin.fcitx5.apply相冲突
+  *因为两者都试图管理同一个配置文件
+  *catppuccin.fcitx5可以导入主题配色可以使用classicui.globalSection.Theme导入主题
+  *也可以禁用inputMethod，设置主题。
+  */
+
+  # 设置rime输入法与美式键盘英文输入法并排序rime第一位
+  i18n.inputMethod.fcitx5.settings.inputMethod = {
+    GroupOrder."0" = "Default";
+    "Groups/0" = {
+      Name = "Default";
+      "Default Layout" = "us";
+      DefaultIM = "keyboard-us";
+    };
+    "Groups/0/Items/0".Name = "keyboard-us";
+    "Groups/0/Items/1".Name = "pinyin";
   };
 
   i18n.inputMethod.fcitx5.settings = {
-    inputMethod = {
-      GroupOrder."0" = "Default";
-      "Groups/0" = {
-        Name = "Default";
-        "Default Layout" = "us";
-        DefaultIM = "keyboard-us";
-      };
-      "Groups/0/Items/0".Name = "keyboard-us";
-      "Groups/0/Items/1".Name = "pinyin";
-    };
     globalOptions = {
+      "Behavior" = {
+        # 关键设置：关闭程序内预编辑，强制使用 Fcitx5 浮窗(能解决 neovide 中无法预编辑的问题)
+        "PreeditEnabledByDefault" = "False";
+      };
       "Hotkey/TriggerKeys" = {"0" = "Super+space";};
     };
+    addons.classicui.globalSection.Theme = "catppuccin-mocha-mauve";
   };
 
+  # catppuccin的flake,option
   catppuccin.fcitx5 = {
     enable = true;
-    flavor = "mocha";
-    accent = "mauve";
-    enableRounded = true;
-    apply = true;
+    flavor = "mocha"; # 主题变体
+    accent = "mauve"; # 强调色
+    enableRounded = true; # 启用圆角
+    apply = false; # 自动配置
   };
 }
