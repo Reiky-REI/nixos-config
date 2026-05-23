c2c03d6440c9c8fd2e515f3de91b75430ff79e5f
reiky <reiky@stud.tjut.edu.cn>
2026-05-24 04:12:27 +0800

    准备开始整理,更改了公钥

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  user_name = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM5SG/6FWHxFnQSRFXfXKgg2kmhLrRJeuw7yCcAZxCgH agenix key";
+  user_name = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMY282QEpZWkXv8oTomNEKt0snDqDYitvBSpY7TdlH5c Reiky-REI@cook";
 in {
   "ai_api_key.age".publicKeys = [user_name];
 }
