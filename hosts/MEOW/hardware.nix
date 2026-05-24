1f86ffbc22e36341432ab0c04df718e9a22d88e6
reiky <reiky@stud.tjut.edu.cn>
2026-05-24 17:13:13 +0800

    fix: clash WM层启动 + btusb参数 + niri config修复

--- hosts/MEOW/hardware.nix
+++ hosts/MEOW/hardware.nix
@@ -10,4 +10,8 @@
   # host-specific 硬件参数
   boot.kernelParams = [ "ahci.mobile_lpm_policy=1" ];
   boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  boot.extraModprobeConfig = ''
+    options btusb enable_autosuspend=0 reset=1
+  '';
 }
