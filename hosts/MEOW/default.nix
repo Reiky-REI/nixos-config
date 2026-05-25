7845d655611c9d0a730e4f4fb655e5e084934771
reiky <reiky@stud.tjut.edu.cn>
2026-05-25 14:39:34 +0800

    chore: 更新 opencode default_agent/plan 配置 + 启用 Waydroid 基础设施

--- hosts/MEOW/default.nix
+++ hosts/MEOW/default.nix
@@ -21,7 +21,7 @@
     shell = pkgs.zsh;
     ignoreShellProgramCheck = true;
     hashedPassword = "$y$j9T$RQ9/Mj/mI5O8AhOnB.3gJ/$mRmKCYV3q7zKoFF1asu5oZNfBNRE4uHDloKQM7Eq5G3";
-    extraGroups = ["wheel" "networkmanager" "audio" "input" "video" "docker" "kvm" "libvirtd"];
+    extraGroups = ["wheel" "networkmanager" "audio" "input" "video" "docker" "kvm" "libvirtd" "waydroid"];
   };
 
   system.stateVersion = "25.05";
